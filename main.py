ba51665deaf6e7a81da756d47ce60013f6d0b265
zylan07 <sathya07rv@gmail.com>
2026-04-24 17:46:20 +0530

    fix cors for vercel

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "https://ascend-app-zeta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
